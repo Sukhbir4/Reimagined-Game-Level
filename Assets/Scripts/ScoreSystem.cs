3f0b4f102ce8133eecc1eec47a44165f582777a6
Ethan <ethan.doolan@ontariotechu.net>
2021-12-02 09:17:11 -0500

    Continued Work on ScoreSystem(WIP)

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,25 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
-    public int playerScore = 0;
+    public static int playerScore = 0;
     public GameObject TimeRemainingUI;
     public GameObject playerScoreUI;
 
-    private void Update()
+    [SerializeField] Text TextVar;
+    [SerializeField] int TimeRemaining;
+    [SerializeField] GameObject deathscreen;
+
+    void Update()
     {
+        TextVar.text = $"Time: {TimeRemaining - Time.realtimeSinceStartup:0}";
+
+        if (TimeRemaining < 0.1)
+        {
+            //Debug.Log("You've run out of time");
+            ////mario = gameObject.GetComponent<mario>();
+            ////deathScreen.SetActive(true);
+        }
+   
         TimeRemainingUI.gameObject.GetComponent<Text>().text = ("Time Left:" + TimeRemaining);
         TimeRemainingUI.gameObject.GetComponent<Text>().text = ("Score:" + playerScore);
     }
 
     // score given once a koopa is killed
-    private void OnTriggerEnter2D(Collider2D KillScore)
+    void OnTriggerEnter2D(Collider2D Score)
     {
-        Debug.Log("Killed Koopa!");
-        CountScore();
+        if(Score.gameObject.name == "Koopa")
+        {
+            playerScore += 10;
+            CountScore();
+        }
+
+        if(Score.gameObject.name == "coin")
+        {
+            playerScore += 10;
+            Destroy(Score.gameObject);
+        }
     }
 
     // points given based on how much time is left upon completion
@@ -27,5 +50,10 @@ public class ScoreSystem : MonoBehaviour
     {
         playerScore = playerScore + (TimeRemaining * 10);
         Debug.Log(playerScore);
+    }
+
+    void PlayerRaycast()
+    {
+
     }
 }
